53ec645b36abff0e439d9286fd3cf80b4d32d2f5
Oscar <96196463+Desarrolloti32@users.noreply.github.com>
2024-10-25 11:21:39 -0600

    Dev reenvio talones sap oscar (#1)
    * feat: se crea proceso para reenviar con peticion PRE-REGISTRO talones a sap
    
    * Create README.md
    
    * Update README.md
    
    * Updated: versiones
    
    * gitignore
    
    * Feat: se cambian las librerias para el log y se modifica ruta del .preperties
    
    * Feat: se agrega validacion para cuando la respuesta tiene una excepcion
    
    * Feat: se modifica la validacion de tiempo en la consulta de talones
    
    * Feat: se elimina codigo comentado
    
    * Update README.md
    
    * Update CHANGELOG.md
    
    * Feat: se cambia el tipo de return en la respuesta del servicio de sap para actualizar el registro en el log y evitar redundancia en la ejecucion
    
    * Feat: se refactoriza bloque if
    
    * Feat: se refactoriza bloque if
    
    * Feat: se cambia la consulta para recuperar talones

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# CronReenvioTalonesSap
+
+## Versión: 1.0.0.2
+- __Ticket/Proyecto:__ N/A
+- __Autor:__  Oscar Eduardo Guerra Salcedo 
+- __Fecha:__ 21/10/2024
+- __Descripción:__ 
+    - Feat: se cambia el tipo de return en la respuesta del servicio de sap para actualizar el registro en el log y evitar redundancia en la ejecucion
---
+++ src/mx/com/castores/util/AccessToken.java
@@ -0,0 +1,17 @@
+package mx.com.castores.util;
+
+public class AccessToken {
+    private String accessToken;
+
+    public AccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+}
